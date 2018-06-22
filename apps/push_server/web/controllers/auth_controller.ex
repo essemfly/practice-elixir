e40a41abc043e56465bae6e216d9b9d9b8bcebb0
seokmin <seokmin@chain.partners>
2018-06-22 15:54:54 +0900

    Simple json dummy data added

--- apps/push_server/web/controllers/auth_controller.ex
+++ apps/push_server/web/controllers/auth_controller.ex
@@ -2,22 +2,24 @@ defmodule PushServer.AuthController do
   use PushServer.Web, :controller
   plug Ueberauth
 
-  alias Ueberauth.Strategy.Helpers
+  # alias Ueberauth.Strategy.Helpers
+
+  def request(conn, _params) do
+    json conn, "Facebook login required"
+  end
 
   def delete(conn, _params) do
-    conn |> json "You have been logged out!"
+    json conn, "You have been logged out!"
   end
 
   def callback(%{assigns: %{ueberauth_failure: _fails}} = conn, _params) do
-    conn |> json "Failed to authenticate."
+    json conn, "Failed to authenticate."
   end
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
     case PushServer.User.find_or_create(auth) do
-      {:ok, user} ->
-        conn |> json user
-      {:error, reason} ->
-        conn |> json reason
+      {:ok, user} -> json conn, user
+      {:error, reason} -> json conn, reason
     end
   end
 end
